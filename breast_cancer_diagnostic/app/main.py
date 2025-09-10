41780e66e8857dc0224db9544a29341ce87d3861
trivikramreddy2006 <147160777+trivikramreddy2006@users.noreply.github.com>
2025-09-10 23:07:05 +0530

    Add files via upload

---
+++ breast_cancer_diagnostic/app/main.py
@@ -0,0 +1,182 @@
+import streamlit as st
+import pickle
+import pandas as pd
+import plotly.graph_objects as go
+import numpy as np
+
+def get_clean_data():
+    data=pd.read_csv(r"D:\DJANGO_COURSE_2.xx (2)\DJANGO_COURSE_2.xx\python_learning\breast_cancer_diagnostic\data\data.csv")
+    
+    data=data.drop(columns=["Unnamed: 32","id"])
+
+    data["diagnosis"]=data["diagnosis"].map({"M":1,"B":0})
+    
+    return data
+
+def add_sidebar():
+        st.sidebar.header("Cell Nuclei Measurements")
+        data=get_clean_data()
+        input_dict={}
+        slider_labels = [
+    # Mean features
+        ("Radius (mean)", "radius_mean"),
+        ("Texture (mean)", "texture_mean"),
+        ("Perimeter (mean)", "perimeter_mean"),
+        ("Area (mean)", "area_mean"),
+        ("Smoothness (mean)", "smoothness_mean"),
+        ("Compactness (mean)", "compactness_mean"),
+        ("Concavity (mean)", "concavity_mean"),
+        ("Concave points (mean)", "concave points_mean"),
+        ("Symmetry (mean)", "symmetry_mean"),
+        ("Fractal dimension (mean)", "fractal_dimension_mean"),
+
+    # SE features
+        ("Radius (SE)", "radius_se"),
+        ("Texture (SE)", "texture_se"),
+        ("Perimeter (SE)", "perimeter_se"),
+        ("Area (SE)", "area_se"),
+        ("Smoothness (SE)", "smoothness_se"),
+        ("Compactness (SE)", "compactness_se"),
+        ("Concavity (SE)", "concavity_se"),
+        ("Concave points (SE)", "concave points_se"),
+        ("Symmetry (SE)", "symmetry_se"),
+        ("Fractal dimension (SE)", "fractal_dimension_se"),
+
+    # Worst features
+        ("Radius (worst)", "radius_worst"),
+        ("Texture (worst)", "texture_worst"),
+        ("Perimeter (worst)", "perimeter_worst"),
+        ("Area (worst)", "area_worst"),
+        ("Smoothness (worst)", "smoothness_worst"),
+        ("Compactness (worst)", "compactness_worst"),
+        ("Concavity (worst)", "concavity_worst"),
+        ("Concave points (worst)", "concave points_worst"),
+        ("Symmetry (worst)", "symmetry_worst"),
+        ("Fractal dimension (worst)", "fractal_dimension_worst"),
+        ]
+        for label,key in slider_labels:
+            input_dict[key]=st.sidebar.slider(
+                 label,min_value=float(0),
+                 max_value=float(data[key].max()),
+                 value=float(data[key].mean())
+            )
+        return input_dict
+
+def get_scaled_values(input_dict):
+    data=get_clean_data()
+    x=data.drop(["diagnosis"],axis=1)
+    scaled_dict={}
+    for key,value in input_dict.items():
+        max_val=x[key].max()
+        min_val=x[key].min()
+        scaled_value=(value-min_val)/(max_val-min_val)
+        scaled_dict[key]=scaled_value
+    return scaled_dict
+
+
+def get_radar_chart(input_data):
+    
+    input_data=get_scaled_values(input_data)
+
+    categories = ['Radius','Texture','Perimeter','Area',
+                  'Smoothness','Compactness','Concavity',
+                  'Concave Points','Symmetry','Fractal Dimension']
+
+    fig = go.Figure()
+
+    fig.add_trace(go.Scatterpolar(
+      r=[
+          input_data["radius_mean"], input_data["texture_mean"], input_data["perimeter_mean"], input_data["area_mean"],
+          input_data["smoothness_mean"], input_data["compactness_mean"], input_data["concavity_mean"], input_data["concave points_mean"],input_data["symmetry_mean"],input_data["fractal_dimension_mean"]
+        ],
+      theta=categories,
+      fill='toself',
+      name='Mean Value'
+    ))
+    fig.add_trace(go.Scatterpolar(
+      r=[
+          input_data["radius_se"], input_data["texture_se"], 
+          input_data ["perimeter_se"], input_data["area_se"],
+          input_data["smoothness_se"], input_data["compactness_se"], input_data["concavity_se"], input_data["concave points_se"],input_data["symmetry_se"],input_data["fractal_dimension_se"]
+      ],
+      theta=categories,
+      fill='toself',
+      name='standard error'
+    ))
+    fig.add_trace(go.Scatterpolar(
+      r=[
+          input_data["radius_worst"], input_data["texture_worst"], 
+          input_data ["perimeter_worst"], input_data["area_worst"],
+          input_data["smoothness_worst"], input_data["compactness_worst"], input_data["concavity_worst"], input_data["concave points_worst"],input_data["symmetry_worst"],input_data["fractal_dimension_worst"]
+      ],
+      theta=categories,
+      fill='toself',
+      name='worst value'
+    ))
+    fig.update_layout(
+    polar=dict(
+    radialaxis=dict(
+      visible=True,
+      range=[0, 1]
+    )),
+    showlegend=True
+    )
+
+    return fig
+
+def add_predictions(input_data):
+
+    model=pickle.load(open(r"D:\DJANGO_COURSE_2.xx (2)\DJANGO_COURSE_2.xx\python_learning\breast_cancer_diagnostic\model\model.pkl","rb"))
+    scaler=pickle.load(open(r"D:\DJANGO_COURSE_2.xx (2)\DJANGO_COURSE_2.xx\python_learning\breast_cancer_diagnostic\model\scaler.pkl","rb"))
+
+    input_array=np.array(list(input_data.values())).reshape(1,-1)
+    # st.write(input_array)
+    input_array_scaled=scaler.transform(input_array)
+
+    st.subheader("Cell Cluster Prediction")
+    st.write("the cell cluster is:")
+    prediction=model.predict(input_array_scaled)
+    
+    if prediction[0] == 0:
+        st.markdown("<div class='result benign'>Benign</div>", unsafe_allow_html=True)
+    else:
+        st.markdown("<div class='result malignant'>Malignant</div>", unsafe_allow_html=True)
+    
+    st.write("probability of being benign is:",model.predict_proba(input_array_scaled)[0][0])
+    
+    st.write("probability of being malicious is:",model.predict_proba(input_array_scaled)[0][1])
+    
+    st.write("this app can assist medical professionals in making a diagnosis,but should not be used as a substitute for a professional diagnosis")
+    
+def main():
+    # print("we are going to build streamlit app here")
+    st.set_page_config(
+        page_title="Breast Cancer Diagnostic App",
+        page_icon=":female-doctor:",
+        layout="wide",
+        initial_sidebar_state="expanded"
+        )
+    # st.write("hello world")
+   
+    with open(r"D:\DJANGO_COURSE_2.xx (2)\DJANGO_COURSE_2.xx\python_learning\breast_cancer_diagnostic\app\styles.css") as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+
+    with st.container():
+        st.title("Breast Cancer Diagnostic App")
+        st.write("An interactive diagnostic tool that analyzes medical features to predict whether a breast tumor is benign or malignant using a Logistic Regression model.")
+
+
+    col1,col2=st.columns([4,1])
+
+    input_data=add_sidebar()
+    # st.write(input_data)
+
+    with col1:
+        radar_chart=get_radar_chart(input_data)
+        st.plotly_chart(radar_chart)
+    with col2:
+        add_predictions(input_data)
+
+if __name__=="__main__":
+    main()
---
+++ breast_cancer_diagnostic/model/main.py
@@ -0,0 +1,49 @@
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, classification_report
+import pickle
+def create_model(data):
+    x=data.drop(columns=["diagnosis"])
+    y=data["diagnosis"]
+
+    ##scale the data
+    scaler=StandardScaler()
+    x=scaler.fit_transform(x)
+
+    ##split the data
+    x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2,random_state=42)
+
+    ##train the model
+    model=LogisticRegression()
+    model.fit(x_train,y_train)
+  
+
+    ##test the model
+    y_pred=model.predict(x_test)
+    print("accuracy of our model:",accuracy_score(y_test,y_pred))
+    print("classification of our model:",classification_report(y_test,y_pred))
+
+    return model,scaler
+
+
+def get_clean_data_():
+    data=pd.read_csv(r"D:\DJANGO_COURSE_2.xx (2)\DJANGO_COURSE_2.xx\python_learning\breast_cancer_diagnostic\data\data.csv")
+    data=data.drop(columns=["Unnamed: 32","id"])
+    data["diagnosis"]=data["diagnosis"].map({"M":1,"B":0})
+    return data
+
+
+def main():
+    data=get_clean_data_()
+    # print(data.head())
+    # print(data.info())
+    model,scaler=create_model(data)
+    with open("model/model.pkl","wb") as f:
+        pickle.dump(model,f)
+    with open("model/scaler.pkl","wb") as f:
+        pickle.dump(scaler,f)
+     
+if __name__=="__main__":
+    main()
